8cd83c815453013b1cfda63c521af055f8092b41
eropka <erop4ik1@gmail.com>
2021-08-28 21:31:47 +0300

    hw-2

---
+++ hw-2/script.js/script.js
@@ -0,0 +1,24 @@
+//1.Переменные для хранения имени, фамилии и возроста пользователя
+let userName = "Егор";
+userSurname = "Бубнов";
+userAge = "23";
+
+//2.Названия для переменных
+
+let newUser = "Новый пользователь";
+shoppingList = "Список покупок";
+userOnline = "Пользователь в сети";
+workerSalary = "Зарплата работника";
+cursorCoordinates = "координаты курсора мыши";
+
+//3.1 Hello world!
+
+let welcome;
+welcome = "Hello world!";
+console.log(welcome);
+
+console.log('Hello world!2');
+
+//3.2 вывод преременной со значением
+console.log(`uerName ${userName}`);
+console.log(`фамилия пользователя, ${userSurname}`);
