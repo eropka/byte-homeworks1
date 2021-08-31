1a03b6369d4f1415829b5b9514a5c5d816ed0cd2
eropka <erop4ik1@gmail.com>
2021-08-31 15:34:11 +0300

    hw-2 rework

--- hw-2/script.js/script.js
+++ hw-2/script.js/script.js
@@ -1,20 +1,20 @@
 //1.Переменные для хранения имени, фамилии и возроста пользователя
 let userName = "Егор";
-userSurname = "Бубнов";
-userAge = "23";
+   userSurname = "Бубнов";
+   userAge = "23";
 
 //2.Названия для переменных
 
 let newUser = "Новый пользователь";
-shoppingList = "Список покупок";
-userOnline = "Пользователь в сети";
-workerSalary = "Зарплата работника";
-cursorCoordinates = "координаты курсора мыши";
+   shoppingList = "Список покупок";
+   userOnline = "Пользователь в сети";
+   workerSalary = "Зарплата работника";
+   cursorCoordinates = "координаты курсора мыши";
 
 //3.1 Hello world!
 
 let welcome;
-welcome = "Hello world!";
+   welcome = "Hello world!";
 console.log(welcome);
 
 console.log('Hello world!2');
